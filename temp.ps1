c084f7a3d3e04fd342e91700ad03ff9a8cf40569
vlabs8 <114287258+vlabs8@users.noreply.github.com>
2022-10-06 23:18:56 +0300

    Update temp.ps1

--- temp.ps1
+++ temp.ps1
@@ -1,14 +1,18 @@
 ﻿New-VHD -ParentPath E:\VM\vhd\w2k22gui.vhdx -Path e:\vm\temp\temp.vhdx -Differencing -Verbose
 New-VM -Name temp -MemoryStartupBytes 4Gb -VHDPath e:\vm\temp\temp.vhdx -Path e:\vm\temp  -Generation 1 -SwitchName Ext
-set-vm -cp
+Set-VMProcessor temp -Count 4
 set-vm temp -CheckpointType Disabled
-start-vm temp
+start-vm temp -verbose
 etsn -VMName temp -Credential administrator
+
+# Can be skipped
 Rename-Computer DC1test -Verbose -Restart -Force
 etsn -VMName temp -Credential administrator
 Get-NetAdapter
 New-NetIPAddress –IPAddress 192.168.1.210 -DefaultGateway 192.168.1.1 -PrefixLength 24 -InterfaceIndex 8
 Set-DNSClientServerAddress –InterfaceIndex 8 –ServerAddresses 127.0.0.1,192.168.1.1
+
+# Install ADDS
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 Import-Module ADDSDeployment
 Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "WinThreshold" -DomainName "vlabs8.com" -DomainNetbiosName "VLABS8" -ForestMode "WinThreshold" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true
