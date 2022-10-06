60bef97c5154b79f7b88c3159ed6291e8b420f2f
vlabs8 <114287258+vlabs8@users.noreply.github.com>
2022-10-06 23:13:29 +0300

    Update temp.ps1

--- temp.ps1
+++ temp.ps1
@@ -12,14 +12,20 @@ Set-DNSClientServerAddress –InterfaceIndex 8 –ServerAddresses 127.0.0.1,192.
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 Import-Module ADDSDeployment
 Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "WinThreshold" -DomainName "vlabs8.com" -DomainNetbiosName "VLABS8" -ForestMode "WinThreshold" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true
+
+#Checkpoint VM
 stop-vm temp -force -verbose
 set-vm temp -CheckpointType Standard
-Set-VM -Name temp -AutomaticCheckpointsEnabled $false
+set-VM -Name temp -AutomaticCheckpointsEnabled $false
 checkpoint-VM -Name temp -SnapshotName 'vlabs domain istalled'
 start-vm temp
+
+# Copy Git Repo
 etsn -VMName temp -Credential vlabs8\administrator
 sl C:\
 git clone https://github.com/vlabs8/w2k22
+
+# Populate with AD Users
 sl C:\w2k22\ADUsers
 .\AdventureWorksSetup.ps1
 Get-ADUser Gates
@@ -30,6 +36,7 @@ exit
 Restore-VMCheckpoint -Name 'vlabs domain istalled' -VMName temp -Confirm:$false
 start-vm temp
 
+# Destroy VM
 stop-vm temp -Force
 Remove-VM temp -Force
 Remove-Item -Recurse e:\vm\temp -Force
