23692ab891ff58c4b5f5fc9e54c246b552bb0825
vlabs8 <114287258+vlabs8@users.noreply.github.com>
2022-10-12 10:40:23 +0300

    Create CreateCSVData.ps1

---
+++ Extras/CreateCSVData.ps1
@@ -0,0 +1,45 @@
+﻿# New Users
+#Create CSV Data
+
+$employees = @(
+
+  [pscustomobject]@{
+
+
+  FirstName = 'Nelly'
+
+  LastName  = 'Eriksen'
+
+  DisplayName = "Nelly J. Eriksen"
+
+  Department = "Research"
+
+  GivenName= "Nelly"
+
+  Title = "Research Manager"
+
+  StreetAddress = "Hafnarbraut 42"
+
+  UserPrincipalName = "Nelly.Eriksen@vlabs8.com"
+  
+  SamAccountName = "Nelly.Eriksen"
+
+  UPN = "Nelly.Eriksen@vlabs8.com"
+
+  OU = "OU=Research,DC=vlabs8,DC=com"
+
+  Email = "Nelly.Eriksen@vlabs8.com"
+
+  City = "Bergen"
+
+  Country = "NO"
+
+  MobilePhone = "5551234" 
+
+  }
+
+  
+
+  )
+
+  $employees | Export-Csv -Path C:\Employees2.csv
