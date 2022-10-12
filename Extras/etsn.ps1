42622fac9efac7489b9a9dfa9bafb6118b48cc5b
vlabs8 <114287258+vlabs8@users.noreply.github.com>
2022-10-12 20:33:01 +0300

    Update etsn.ps1

--- Extras/etsn.ps1
+++ Extras/etsn.ps1
@@ -3,3 +3,7 @@ etsn -VMName DC1 -Credential $DomainCredentials
 etsn -VMName DC2 -Credential $DomainCredentials
 etsn -VMName DC-CORE -Credential $DomainCredentials
 etsn -VMName CL1 -Credential $DomainCredentials
+
+$20742BDomainCredentials=Get-Credential adatum\administrator
+etsn -VMName 20742B-LON-DC1 -Credential $20742BDomainCredentials
+ 
