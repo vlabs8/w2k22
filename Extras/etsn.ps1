cb27acb14196df4db5815d5e86e3c2533405bf87
vlabs8 <114287258+vlabs8@users.noreply.github.com>
2022-10-12 10:40:16 +0300

    Create etsn.ps1

---
+++ Extras/etsn.ps1
@@ -0,0 +1,5 @@
+﻿$DomainCredentials=Get-Credential vlabs8\administrator
+etsn -VMName DC1 -Credential $DomainCredentials
+etsn -VMName DC2 -Credential $DomainCredentials
+etsn -VMName DC-CORE -Credential $DomainCredentials
+etsn -VMName CL1 -Credential $DomainCredentials
