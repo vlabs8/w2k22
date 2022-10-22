b3f5cb3297cd4512587ef748abfc83569edc9420
vlabs8 <114287258+vlabs8@users.noreply.github.com>
2022-10-22 11:04:00 +0300

    Create 6.HyperV.ps1

---
+++ 6.HyperV.ps1
@@ -0,0 +1,8 @@
+﻿# To enable nested virtualization in a virtual machine named DemoVM:
+Set-VMProcessor -VMName DemoVM -ExposeVirtualizationExtensions $true
+
+# To create a NAT virtual switch, use the following Windows PowerShell command:
+New-VMSwitch -Name “NATSwitch” -SwitchType NAT -NATSubnetAddress 172.16.1.0/24
+	
+# To check a virtual machine’s configuration version
+Get-VM * | Format-Table Name, Version
